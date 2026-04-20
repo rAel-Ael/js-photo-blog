211a2bb36e4deb82a0725e45c1a1a0c9fb1e525f
rAel-Ael <alediru9267@gmail.com>
2026-04-20 19:09:47 +0200

    pin che sparisce, bottone exit per il primo piano dell'img e un background opaco

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiURL = 'https://lanciweb.github.io/demo/api/pictures/';
 const carta = document.querySelector('#carta');
 const container = document.querySelector('#card-container');
 const focus = document.querySelector('#prev-foto');
+const end = document.querySelector('#chiudi');
 //selezionare il nodo html dove voglio stampare le card
 
 
@@ -21,34 +22,40 @@ fetch(apiURL)
       pictureContainer.classList.add('scatola-card' , 'scatola-card' , 'col-12' , 'col-md-6', 'col-lg-4', 'd-flex', 'justify-content-center');
       html = `
                 <div class="carta col-lg-6 mb-4" data-gallery="panorama">
-                    <img src="img/pin.svg" class="pin" alt="puntina">
+                    
                     <div class="polaroid">
                         <div class="img">
+                          <img src="img/pin.svg" class="pin" alt="puntina">
                           <img class="foto" src="${pic.url}" alt="${pic.title}">
                         </div>
                         <div class="info">
                             <span class="date">${pic.date}</span>
-<p>${pic.title}</p>
+                              <p>${pic.title}</p>
                         </div>
                     </div>
                 </div>
       `;
       //qui
       pictureContainer.innerHTML = html
-
       pictureContainer.addEventListener("click", function () {
         console.log(pic.url); 
         
         let divPrev = document.getElementById('prev-foto');
         divPrev.classList.remove('d-none');
-
-        let img = document.getElementById('foto');
+        let img = document.getElementById('anteFoto');
         img.src = pic.url
-
-
       });
       
       container.appendChild(pictureContainer)
+      
+      end.addEventListener("click", function () {
+        
+        let divPrev = document.getElementById('prev-foto');
+        let btnChiudi = document.getElementById('chiudi');
+        divPrev.classList.add('d-none');
+      })
+      
+      
 
       
     }) 
